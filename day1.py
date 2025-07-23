878ba28e735471385857cf800b99bc755e311dcb
Hanshitha Killi <killihanshitha7@gmail.com>
2025-07-23 20:55:22 +0530

    🔧 Updated code with new feature/fix

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
+# 1st task: List Comprehensions
+
 if __name__ == '__main__':
     x = int(input())
     y = int(input())
@@ -12,3 +14,16 @@ result = [[i, j, k]
           if i + j + k != n]
 
 print(result)
+
+# 2nd task: Find the Runner-Up Score!
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    
+    unique_scores = list(set(arr))          
+    unique_scores.sort(reverse=True)        
+    
+    print(unique_scores[1])                 
+
